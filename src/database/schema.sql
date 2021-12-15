39bca789cb39cb11674722576293b8a906a22977
Hilton1 <josehilton02@gmail.com>
2021-12-15 11:04:18 -0300

    adding schema.sql to the project database

---
+++ src/database/schema.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE supermarkethr;
+
+CREATE EXTENSION "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS products(
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4() PRIMARY KEY,
+  barcode VARCHAR NOT NULL,
+  name VARCHAR NOT NULL,
+  price DECIMAL NOT NULL,
+  quantity INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS access_level(
+  id SERIAL NOT NULL UNIQUE PRIMARY KEY,
+  description VARCHAR NOT NULL
+);
+
+INSERT INTO access_level(description) VALUES('INVENTORY CONTROLLER');
+INSERT INTO access_level(description) VALUES('SELLER');
+
+CREATE TABLE IF NOT EXISTS employee(
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4() PRIMARY KEY,
+  id_access_level INT NOT NULL DEFAULT 2,
+  name VARCHAR NOT NULL,
+  FOREIGN KEY(id_access_level) REFERENCES access_level(id)
+);
